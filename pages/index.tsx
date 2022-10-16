9a28ad2fd84a4b00036159750ec7f18449456f51
pavanleton <pavankumarkpkp@gmail.com>
2022-10-16 21:59:06 +0530

    update: sign with wallet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
 import type { NextPage } from 'next'
+import {useWallet} from "hooks/useWallet";
+import {getENSNameByAddress} from "utils/getENSNameByAddress";
 
 const Home: NextPage = () => {
+
+  const {signWithWallet, connectWallet} = useWallet()
+
+  const handleSignIn = async () => {
+      try {
+          await connectWallet()
+          const signRes = await signWithWallet()
+      } catch (error) {
+          console.error(error)
+      }
+  }
+
   return (
       <div className="flex items-center justify-center h-screen">
           <div>
               <button
                   type="submit"
                   className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  onClick={handleSignIn}
               >
                   Continue with your wallet
               </button>
