94a1958e4a8bc7586eeada7f246a49d9fde8ae87
pavanleton <pavankumarkpkp@gmail.com>
2022-10-17 00:46:18 +0530

    update: supabase config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,62 @@
-import type { NextPage } from 'next'
-import {useRouter} from 'next/router'
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 // HOOKS
-import {useWallet} from "hooks/useWallet";
+import { useWallet } from "hooks/useWallet";
 
 // UTILS
-import {getENSNameByAddress} from "utils/getENSNameByAddress";
+import { getENSNameByAddress } from "utils/getENSNameByAddress";
 
 const Home: NextPage = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
 
-  const {signWithWallet, connectWallet} = useWallet()
-  const router = useRouter()
+    const { signWithWallet, connectWallet } = useWallet();
+    const router = useRouter();
 
-  const handleSignIn = async () => {
-      try {
-          await connectWallet()
-          const signRes = await signWithWallet()
+    const handleSignIn = async () => {
+        try {
+            setIsLoading(true);
+            await connectWallet();
+            const signRes = await signWithWallet();
 
-          await fetch('/api/auth', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-                  message: signRes?.message as string,
-                  signature: signRes?.signature as string,
-              },
-              body: JSON.stringify({
-                  address: signRes?.account as string,
-              })
-          })
+            await fetch("/api/auth", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    message: signRes?.message as string,
+                    signature: signRes?.signature as string,
+                },
+                body: JSON.stringify({
+                    address: signRes?.account as string,
+                }),
+            });
 
-          sessionStorage.setItem('auth-app-session', 'true')
-          router.push('/profile')
-      } catch (error) {
-          console.error(error)
-      }
-  }
+            sessionStorage.setItem("auth-app-session", "true");
+            setIsLoading(false);
+            router.push("/profile");
+        } catch (error) {
+            setIsLoading(false);
+            setError(error?.message || "Internal Server Error");
+        }
+    };
 
-  return (
-      <div className="flex items-center justify-center h-screen">
-          <div>
-              <button
-                  type="submit"
-                  className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  onClick={handleSignIn}
-              >
-                  Continue with your wallet
-              </button>
-          </div>
-      </div>
-  )
-}
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div>
+                <p className={"text-red-600 text-sm font-semibold"}>{error}</p>
+                <button
+                    type="submit"
+                    className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
+                    onClick={handleSignIn}
+                    disabled={isLoading}
+                >
+                    { isLoading ? "Loading..." : "Continue with your wallet"}
+                </button>
+            </div>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
