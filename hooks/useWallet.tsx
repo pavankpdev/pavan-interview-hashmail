18f171e2e15c914236ac52727986e3f8221e58ed
pavanleton <pavankumarkpkp@gmail.com>
2022-10-17 00:10:24 +0530

    update: sign message with web3js

--- hooks/useWallet.tsx
+++ hooks/useWallet.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useState } from "react";
+import Web3 from "web3";
 
 // This Hook will be used to handle certain tasks that involves wallets such as connecting to a wallet,
 // disconnecting from a wallet, getting the current wallet address, etc.
@@ -20,14 +21,13 @@ export function useWallet() {
 
         if(typeof window?.ethereum !== 'undefined') {
             try {
-                const ethers = require('ethers')
-                const provider = new ethers.providers.Web3Provider(window?.ethereum)
-                const signer = provider.getSigner();
+                const web3 = new Web3(window.ethereum);
                 const message = Date.now().toString();
-                const messageHash = ethers.utils.hashMessage(message);
-                const signature = await signer.signMessage(messageHash);
-                const accounts = await signer.getAddress();
-                return { signature, account: accounts };
+                const messageHash = web3.utils.sha3(message) as string
+                const accounts = await web3.eth.getAccounts();
+                const signature = await web3.eth.personal.sign(messageHash, accounts[0], "");
+
+                return { signature, account: accounts[0], message: messageHash };
             } catch (err) {
                 console.log(err)
                 throw err
