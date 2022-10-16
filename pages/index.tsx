dcea0cf8800665ca5e1c0554e6f0667c1fca73a9
pavanleton <pavankumarkpkp@gmail.com>
2022-10-17 00:22:40 +0530

    update: redirect to profile page after auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,36 @@
 import type { NextPage } from 'next'
+import {useRouter} from 'next/router'
+
+// HOOKS
 import {useWallet} from "hooks/useWallet";
+
+// UTILS
 import {getENSNameByAddress} from "utils/getENSNameByAddress";
 
 const Home: NextPage = () => {
 
   const {signWithWallet, connectWallet} = useWallet()
+  const router = useRouter()
 
   const handleSignIn = async () => {
       try {
           await connectWallet()
           const signRes = await signWithWallet()
+
+          await fetch('/api/auth', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  message: signRes?.message as string,
+                  signature: signRes?.signature as string,
+              },
+              body: JSON.stringify({
+                  address: signRes?.account as string,
+              })
+          })
+
+          sessionStorage.setItem('auth-app-session', 'true')
+          router.push('/profile')
       } catch (error) {
           console.error(error)
       }
