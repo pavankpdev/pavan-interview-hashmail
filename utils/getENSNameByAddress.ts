1c23ad948b2b794e195345dbbc3a3a0894e81a07
pavanleton <pavankumarkpkp@gmail.com>
2022-10-17 00:24:03 +0530

    update: remove hardcoded RPC

--- utils/getENSNameByAddress.ts
+++ utils/getENSNameByAddress.ts
@@ -3,7 +3,7 @@ import {ethers} from "ethers";
 export const getENSNameByAddress = async (address: string) => {
     try {
         const provider = new ethers.providers.JsonRpcProvider(
-            process.env.NEXT_PUBLIC_ETH_RPC_URL || 'https://goerli.infura.io/v3/0633996eda2b45f3b5359bf54f7956d4'
+            process.env.NEXT_PUBLIC_ETH_RPC_URL
         );
         return provider.lookupAddress(address);
     } catch (err: any) {
